02f49afbf1f2999c6ee008e299a16d03a7099afe
Jacqueline Cardozo <jackie@Jacquelines-MacBook-Pro.local>
2019-04-08 11:28:19 -0300

    Updatins styling

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <template>
   <div id="app">
     <div class="container">
-      <h1>Fancy Quote {</h1>
+      <h1>Fancy Quote <span class="highlight">}</span></h1>
+      <div id="fancy-quote-1"><span>To be amused at </span><span>what you read - </span><span>that is the great </span><span>spring of quotation. </span><span>Charles Edward Montague</span></div>
       <div class="content">
         <div class="form">
-          <div v-if="currentStep === step[0]">
+          <div v-if="currentStep === step[0]" class="step">
             <div class="form__item">
               <label for="quote">Quote:</label>
               <textarea 
@@ -33,10 +34,10 @@
             </div>
           </div>
           
-          <div v-if="currentStep === step[1]">
+          <div v-if="currentStep === step[1]" class="step">
             <div class="form__item">
               <label for="fadeness">Fadeness:</label>
-              <div>
+              <div class="form__range">
                 <input 
                   type="range" 
                   value="" 
@@ -51,7 +52,7 @@
             </div>
             <div class="form__item">
               <label for="speed">Speed:</label>
-              <div>
+              <div class="form__range">
                 <input 
                   type="range" 
                   value="" 
@@ -66,7 +67,7 @@
             </div>
             <div class="form__item">
               <label for="split">Split:</label>
-              <div>
+              <div class="form__range">
                 <input 
                   type="range" 
                   value="" 
@@ -80,17 +81,29 @@
               </div>
             </div>
           </div>
-          <div class="form__item" v-if="currentStep === step[2]">
+          <div class="form__item step" v-if="currentStep === step[2]">
             <label for="generated">Generated code:</label>
             <textarea 
               v-model="code"
-              name="generated">
+              name="generated"
+              ref="generated"
+              @click="copyContent">
             </textarea>
+            <div :class="['copy', {'copy--show' : showCopiedTip}]">
+              <span>Copied!</span>
+            </div>
           </div>
-          <button @click.prevent="nextStep">{{ currentStepLabel }}</button>
+          <div class="control">
+            <button v-show="currentStep !== 0" @click.prevent="previousStep" class="previous">
+              <span>Previous</span>
+            </button>
+            <button v-show="currentStep < 2" @click.prevent="nextStep" class="next">
+              <span>Next</span>
+            </button>
+          </div>
         </div>
 
-        <div>
+        <div class="output">
           <h3>Output</h3>
           <div v-html="this.code"></div>
         </div>
@@ -113,38 +126,44 @@ export default {
       quotation: true,
       step: [0, 1, 2],
       currentStep: 0,
-      currentStepLabel: 'Next Step',
-      stepLabels: ['Next Step', 'Generate', 'Start Again'],
-      generateCode: false
+      generateCode: false,
+      showCopiedTip: false
     }
   },
   mounted(){
-    this.generate()
+    this.generate(false)
   },
   methods: {
     nextStep(){
-      this.currentStep = this.currentStep === 2 ? 0 : this.currentStep + 1
-      this.currentStepLabel = this.stepLabels[this.currentStep]
+      this.currentStep += 1
     },
-    generate(){
+    previousStep(){
+      this.currentStep -= 1
+    },
+    generate(noAnimation){
       const sentences = this.breakIntoSentences(this.quote)
       const output = this.splitSentences(sentences, Number(this.split))
+      const speed = Number(this.speed)
       let spans = ''
       let css = ''
       let animation = ''
       output.forEach((item, i) => {
         spans += `<span>${i === 0 && this.quotation ? '"' : ''}${item}${i === (output.length-1) && this.quotation ? '"' : ''} </span>`
-        css += `#fancy-quote span:nth-child(${i+1}){animation: fade-in ${this.speed}ms ${this.speed * i}ms forwards;}`
+        css += `#fancy-quote span:nth-child(${i+1}){animation: fade-in ${speed}ms ${speed * i}ms forwards;}`
       })
-      css += `#fancy-quote span:nth-child(${output.length+1}){animation: fade-in ${this.speed}ms ${this.speed * (output.length)}ms forwards;}`
+      const authorSpeed = speed > 500 ? speed : speed + 1000
+      css += `#fancy-quote span:nth-child(${output.length+1}){animation: fade-in ${ authorSpeed }ms ${speed * output.length + (authorSpeed / 2)}ms forwards;}`
       spans += `<span>${this.author}</span>`
 
       for (let i = 0; i <= this.fadeness; i++) {
         animation += `${(i / this.fadeness) * 100}% {opacity: ${i / this.fadeness};}`
       }
-
-      let styles = `<style>@keyframes fade-in{${animation}}#fancy-quote{--font-quote: Serif; font-family: var(--font-quote); font-size: 28px; max-width: 400px;}#fancy-quote span{opacity: 0;}${css}#fancy-quote span:last-child{display: block; font-family: var(--font-quote); font-weight: 100; font-size: 0.75em; letter-spacing: -1px;}#fancy-quote span:last-child:before{content: "-"; padding-right: 10px;}</style>`
-
+      let opacity = 0
+      if (!noAnimation) {
+        animation= ''
+        opacity = 1
+      }
+      let styles = `<style>@keyframes fade-in{${animation}}#fancy-quote span{opacity: ${opacity};}${css}#fancy-quote span:last-child{display: block; font-weight: 100; font-size: 0.75em; letter-spacing: -1px;}#fancy-quote span:last-child:before{content: "-"; padding-right: 10px;}</style>`
       let html = `<div id="fancy-quote">${spans}</div>`
       this.code = `${styles}${html}`
     },
@@ -152,7 +171,6 @@ export default {
       return s.split(/(?<=[.?!;])\s*/g)
     },
     splitSentences(arr, number){
-      console.log(arr, number)
       if(number === 0) {
         return arr
       }
@@ -170,16 +188,29 @@ export default {
       const a = str.split(' '), b = [];
       while(a.length) b.push(a.splice(0,splitLength).join(' '));
       return b;
-    }
+    },
+    copyContent() {
+      const textarea = this.$refs.generated;
+      textarea.select();
+      document.execCommand("copy");
+      this.showCopiedTip = true;
+      setTimeout(() => {
+        this.showCopiedTip = false;
+      }, 3000);
+    },
   }
 }
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css?family=Titillium+Web:200');
+
 :root{
+  --highlight-color: #269459;
   --border-color: rgba(0, 0, 0, 0.1);
   --bg-color: rgba(0, 0, 0, 0.01);
   --font-color: #333;
+  --font-quote: 'Titillium Web', serif;
 }
 
 #app {
@@ -188,7 +219,12 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: var(--font-color);
-  margin-top: 60px;
+}
+
+@media (min-width: 800px) {
+  #app {
+    margin-top: 60px;
+  }
 }
 
 .container {
@@ -212,40 +248,22 @@ export default {
   }
 }
 
-@keyframes floating1 {
-    from { transform: translate(0,  0px); }
-    65%  { transform: translate(-1px, 15px); }
-    to   { transform: translate(0, -0px); }    
-}
-@keyframes floating2 {
-    from { transform: translate(0,  0px); }
-    33%  { transform: translate(-2px, 10px); }
-    66%  { transform: translate(-5px, 15px); }
-    to   { transform: translate(0, -0px); }    
-}
-@keyframes floating3 {
-    from { transform: translate(0,  0px); }
-    33%  { transform: translate(-1px, 10px); }
-    66%  { transform: translate(-3px, 15px); }
-    to   { transform: translate(0, -0px); }   
-}
-
-.floating-one{
-  animation: floating1 10s infinite ease-in-out;
-}
-
-.floating-two{
-  animation: floating2 10s infinite ease-in-out;
-}
-
-.floating-three{
-  animation: floating3 10s infinite ease-in-out;
-}
-
 .content{
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  grid-column-gap: 50px;
+  min-height: 280px;
+}
+
+@media (min-width: 800px) {
+  .content{
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-column-gap: 50px;
+    align-items: center;
+    min-height: 280px;
+  }
+}
+
+.highlight{
+  color: var(--highlight-color);
 }
 
 .form__item{
@@ -260,8 +278,15 @@ export default {
   text-align: right;
 }
 
-body textarea,
-body input{
+.form__range{
+  display: grid;
+  grid-template-columns: 1fr 50px;
+  grid-column-gap: 10px;
+  align-items: center;
+}
+
+.form textarea,
+.form input{
   border: 1px solid var(--border-color);
   background-color: var(--bg-color);
   font-size: 12px;
@@ -271,8 +296,124 @@ body input{
   color: var(--font-color);
 }
 
-body textarea{
+.form textarea{
   resize: inherit;
   min-height: 100px;
+}
+
+.form button{
+  position: relative;
+  background: transparent;
+  border: 0;
+  cursor: pointer;
+  padding: 6px 15px;
+}
+
+.form button.next:after{
+  content: "";
+  position: absolute;
+  top: 8px;
+  right: -5px;
+  width: 0;
+  height: 0;
+  border-top: 7px solid transparent;
+  border-left: 9px solid var(--highlight-color);
+  border-bottom: 7px solid transparent;
+}
+
+.form button.next:before{
+  content: "";
+  position: absolute;
+  top: 8px;
+  right: 1px;
+  width: 17px;
+  height: 14px;
+  background-color: #fff;
+  z-index: 1;
+  border-radius: 50%;
+}
+
+.form button.previous:after{
+  content: "";
+  position: absolute;
+  top: 8px;
+  left: -5px;
+  width: 0;
+  height: 0;
+  border-top: 7px solid transparent;
+  border-right: 9px solid var(--highlight-color);
+  border-bottom: 7px solid transparent;
+}
+
+.form button.previous:before{
+  content: "";
+  position: absolute;
+  top: 8px;
+  left: 1px;
+  width: 17px;
+  height: 14px;
+  background-color: #fff;
+  z-index: 1;
+  border-radius: 50%;
+}
+
+.form button span{
+  position: relative;
+  z-index: 2;
+}
+
+.control{
+  display: grid;
+  grid-template-columns: 80px 1fr 80px;
+}
+
+.control .previous{
+  grid-column: 1;
+}
+
+.control .next{
+  grid-column: 3;
+}
+
+.copy{
+  grid-column: 2;
+  text-align: right;
+  opacity: 0;
+  font-size: .75rem;
+  padding: 5px 0;
+  transition: .5s opacity ease-in-out;
+}
+
+.copy--show{
+  opacity: 1;
+}
+
+.output #fancy-quote{
+  font-family: var(--font-quote);
+  font-size: 1.5rem;
+  line-height: 2.25rem;
+}
+
+.output h3{
+  margin-top: 0;
+}
+
+@keyframes fancy-quote-1{
+  0% {opacity: 0;}
+  100% {opacity: 1;}
+}
+
+#fancy-quote-1 span{opacity: 0;}
+#fancy-quote-1 span:nth-child(1){animation: fancy-quote-1 900ms 0ms forwards;}
+#fancy-quote-1 span:nth-child(2){animation: fancy-quote-1 900ms 900ms forwards;}
+#fancy-quote-1 span:nth-child(3){animation: fancy-quote-1 900ms 1800ms forwards;}
+#fancy-quote-1 span:nth-child(4){animation: fancy-quote-1 900ms 2700ms forwards;}
+#fancy-quote-1 span:nth-child(5){animation: fancy-quote-1 900ms 4050ms forwards;}
+#fancy-quote-1 span:last-child{display: block; font-weight: 100; font-size: 0.75em; letter-spacing: -1px;}
+#fancy-quote-1 span:last-child:before{content: "-"; padding-right: 10px;}
+
+body #fancy-quote,
+#fancy-quote-1{
+  margin: 10px auto;
 }
 </style>
